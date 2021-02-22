6d8cf3b3729c69559979998e2ec23ead03781acc
DefunctLizard <grantshandy@gmail.com>
2021-02-22 15:11:28 -0700

    add basic GTK4 boilerplate things

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,27 @@
-fn main() {}
+use gtk::prelude::*;
+use std::env::args;
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+
+    window.set_title(Some("Ride"));
+    window.set_default_size(1150, 650);
+
+    let label = gtk::Label::new(Some("Content Here!"));
+
+    window.set_child(Some(&label));
+
+    window.show();
+}
+
+fn main() {
+    let application =
+        gtk::Application::new(Some("org.skylinecc.Ride"), Default::default())
+            .expect("Initialization failed...");
+
+    application.connect_activate(|app| {
+        build_ui(app);
+    });
+
+    application.run(&args().collect::<Vec<_>>());
+}
